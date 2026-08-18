0a872b38403329dcf9a740b087c2cbcc791f6553
MATHEUS RAMOS BAGATTOLI <matheus_bagattoli@estudante.sesisenai.org.br>
2026-08-17 21:32:45 -0300

    Commit inicial

---
+++ src/test/java/com/senai/Zheiri_Solutions/ZheiriSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.Zheiri_Solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZheiriSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
